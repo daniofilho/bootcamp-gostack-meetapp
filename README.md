579b24befbd78e0f53abd80d851513c03437401c
Dânio Filho <daniofilho@me.com>
2019-08-22 20:04:59 -0300

    desafio 2 ok

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Meetapp
 
 Projeto Bootcamp GoStack - Rocketseat
-
-## Docker database
-
-docker run --name meetapp_database -e POSTGRES_USER=meetapp -e POSTGRES_PASSWORD=meetapp -d postgres
--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'ce01095e04bfb96f727fffbfd5f4ec9d', // string único, gere aqui https://www.md5online.org
+  secret: '04d904c27150794896a98a06324e7dc8',
   expiresIn: '7d',
 };
